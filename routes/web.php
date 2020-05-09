69f040e58702c65fbafee2f57ab8c3c8a6648495
keisuke <rememberthatk@icloud.com>
2020-05-10 08:40:23 +0900

    [master] add auth

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('book', 'BookController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
